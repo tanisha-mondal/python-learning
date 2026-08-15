18ae8b6731fbf1672e6c4b9ca13146fe057e3d7c
Tanuka <tanukamondal01@gmail.com>
2026-08-15 22:59:46 +0530

    day 1 code

---
+++ Chapter1/module.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+# print("Printing Jokes...")
+
+# This is ghatiya
+joke = pyjokes.get_joke()
+print(joke)
+
+# I am compelled to 
+# do this
---
+++ Chapter1/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+I am compelled to 
+# do this
